15aa30f39ec6ad8080fffcc008093f3e4767d5f2
Wajeeha Batool <wb140507@gmail.com>
2025-12-07 13:34:41 +0500

    Basic codes
    Here are some basic codes of c++.

---
+++ Lab 1/task 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"Welcome to the C++ lab!"<<endl;
+	cout<<"You will learn basic output today."<<endl;
+	cout<<"Let's start with simple examples today."<<endl;
+	
+	
+	 return 0;
+	
+	
+}
